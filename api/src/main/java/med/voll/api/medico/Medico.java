327af98432441696e8a4e5be713972546d3d9fcc
JorginhoO <jorge.infoo88@gmail.com>
2023-05-24 11:46:58 -0300

    Entity jPA

---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,31 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Table(name = "tb_medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor // gerar construtor Default sem argumentos, JPA exige em todas entidades
+@AllArgsConstructor // construtor para receber todos os campos
+@EqualsAndHashCode(of = "id") //gerar o equals com o id
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded // considera outra classe na mesma tabela
+    private Endereco endereco;
+
+}
